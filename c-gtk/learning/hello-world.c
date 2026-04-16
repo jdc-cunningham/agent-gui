8ef8d651c76f783488f025aa0ea0424c1bbf0796
Jacob Cunningham <jcun4128@gmail.com>
2026-04-15 21:02:12 -0500

    baby's first c gtk window

--- c-gtk/learning/hello-world.c
+++ c-gtk/learning/hello-world.c
@@ -29,7 +29,7 @@ main(int argc, char **argv)
     int status;
     app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-    status = g_application_run(G_APPLICATION(app), arc, argv);
+    status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
     return status;
 }
